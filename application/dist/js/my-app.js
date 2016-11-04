d3f0e384c2c6477ce56ea3139cad17f9121ee906
jonathantobi <jonathan_tobi@live.com>
2016-11-04 16:45:01 -0300

    account page, transaction page created

--- application/dist/js/my-app.js
+++ application/dist/js/my-app.js
@@ -6,7 +6,8 @@ var mainView = myApp.addView('.view-main');
 
 myApp.onPageInit('index', function (page) {
 
-	mainView.router.loadPage('views/login.html');
+    // mainView.router.loadPage('views/login.html');
+	mainView.router.loadPage('views/account.html');
     console.log('triggered: index');
 
 }).trigger();
