58f3ec790e0e42c1eac8561327590bc8a5518f22
Developer <Developer>
2016-11-04 15:43:54 -0300

    login page gezet

--- application/dist/js/my-app.js
+++ application/dist/js/my-app.js
@@ -5,6 +5,6 @@ var $$ = Dom7;
 var mainView = myApp.addView('.view-main');
 
 myApp.onPageInit('index', function (page) {
-    // mainView.router.loadPage();
+	mainView.router.loadPage('views/login.html');
     console.log('triggered: index');
 }).trigger();
