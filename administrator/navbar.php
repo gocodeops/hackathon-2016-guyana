498192d07d4e945bce2268aa277be7d6f79c3b0c
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2016-11-04 13:55:56 -0400

    added administrator ui

---
+++ administrator/navbar.php
@@ -0,0 +1,23 @@
+<body>
+<!-- Simple splash screen-->
+<div class="splash"> <div class="color-line"></div><div class="splash-title"><h1>Administrator</h1><p>Pension Payment System</p><img src="images/loading-bars.svg" width="64" height="64" /> </div> </div>
+<!--[if lt IE 7]>
+<p class="alert alert-danger">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<!-- Header -->
+<div id="header">
+    <div class="color-line">
+    </div>
+    <div id="logo" class="light-version">
+        <span>
+            <center>NAIS</center>
+        </span>
+    </div>
+    <nav role="navigation">
+        <div class="header-link hide-menu"><i class="fa fa-bars"></i></div>
+        <div class="small-logo">
+            <span class="text-primary">NAIS</span>
+        </div>
+    </nav>
+</div>
