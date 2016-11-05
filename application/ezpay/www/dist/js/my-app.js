2edb339f3e6b5ba9c46926966bfeb1a5e11b942d
jonathantobi <jonathan_tobi@live.com>
2016-11-05 10:11:59 -0300

    balance naar currency, routing id moet naar desbetreffende wachtwoord page

--- application/ezpay/www/dist/js/my-app.js
+++ application/ezpay/www/dist/js/my-app.js
@@ -8,7 +8,7 @@ var mainView = myApp.addView('.view-main');
 
 myApp.onPageInit('index', function (page) {
 
-    mainView.router.loadPage('views/transactions.html');
+    mainView.router.loadPage('views/login.html');
 	// mainView.router.loadPage('views/account.html');
     // console.log('triggered: index');
 
@@ -25,3 +25,77 @@ function zero(val){
 };
 
 var notification_date;
+
+function updateNotificationDate(){
+	var d = new Date();
+	var ye = d.getFullYear();
+	var mo = zero(d.getMonth()+1);
+	var dy = zero(d.getDate());
+	var hr = zero(d.getHours());
+	var mi = zero(d.getMinutes());
+	var se = zero(d.getSeconds());
+
+	notification_date = ye+'-'+mo+'-'+dy+' '+hr+':'+mi+':'+se;
+	localStorage.setItem("notification_date", notification_date);
+	console.log('notification_date updated: '+notification_date);
+}
+
+if (localStorage.getItem("notification_date") == null) {
+	updateNotificationDate();
+}else{
+	notification_date = localStorage.getItem("notification_date");
+	console.log('got date out of localStorage');
+}
+
+// deviceready function for cordova
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// do something after cordova ready
+function onDeviceReady() {
+	// console.log('deviceready');
+
+	setInterval(function (){
+		// console.log("interval running ...");
+
+		function checkTransactions(){
+			console.log('notification_date: '+notification_date);
+
+			if (localStorage.getItem('receiver_id') != null) {
+
+					// check op ad
+					$$.post("http://gocodeops.com/hackathon_guyana_app/public/transactions"+notification_date,
+					 function(data){
+					 	var data = JSON.parse(data);
+						// console.log('ads check ran with result: '+data);
+						if (data != 0) {
+							cordova.plugins.notification.local.schedule({
+						        id: 1,
+						        title: "Payment!",
+						        text: "Payment Received, why don/'t you go check it out!",
+						        icon: "file://logo.png",
+						    });
+						    // cordova.plugins.notification.badge.increase();
+						}else{
+							console.log('no new transactions');
+						}	
+					});
+
+			}
+				
+		}
+
+		setTimeout(function(){ checkTransactions(); }, 1000);
+
+		setTimeout(function(){ updateNotificationDate(); }, 30000);
+
+    }, 10000);
+
+	// notification click listener
+    cordova.plugins.notification.local.on("click", function (notification, state) {
+    	    // check which notification was clicked
+    	    // notification.id
+		    mainView.router.loadPage('views/msp_nieuws.html');
+	    
+	}, this)
+
+}
