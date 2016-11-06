ed7ca0f10ed40f0580a3093eb04399f296b01889
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2016-11-06 03:40:15 -0400

    gewerkt aan de invoices

--- backend/public/invoices.php
+++ backend/public/invoices.php
@@ -1,7 +1,7 @@
 <?php
     /**
      * TODO:
-     *
+     * - get invoice by receiver_id
      * DONE:
      * - update invoice
      */
@@ -12,16 +12,22 @@
         if ($data['status'] == 1) {
             $paymentData = array(
                 'amount' => $data['amount'],
-                'sender_id' => $data['sender_id'],
-                'receiver_id' => $data['receiver_id'],
+                'sender_id' => $data['receiver_id'],
+                'receiver_id' => $data['sender_id'],
                 );
-            DB::table('payments')->insert($paymentData);
+            // DB::table('payments')->insert($paymentData);
             $delete = DB::table('invoices')->where('id', $invoice_id)->delete();
             // print_r(json_encode($query));
             print_r(1);
         } else {
-            DB::table('payments')->where('id', $invoice_id)->update(array('status' => 0));
+            DB::table('invoices')->where('id', $invoice_id)->update(array('status' => 0));
             print_r(0);
         }
     });
+
+    $app->get('/invoice/get/{receiver_id}', function($request, $response, $args){
+        $query = DB::table('view_invoices')->where('receiver_id', $args['receiver_id'])->get();
+        print_r(json_encode($query));
+    });
+
 ?>
